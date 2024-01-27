cebfa33791a1c86b3b09eda34a41b7e1f111802a
aditipokharel0 <128813746+aditipokharel0@users.noreply.github.com>
2024-01-27 10:10:13 +0545

    Delete Portfolio/week 6 aditi directory

--- Portfolio/week 6 aditi/604.py
+++
@@ -1,9 +0,0 @@
-def simple_encrypt(message):
-    encrypted_message = message.replace(" ", "")[::-1]
-    return encrypted_message
-
-originalmessage = "Hello, world! This is a simple encryption example."
-encryptedmessage = simple_encrypt(original_message)
-
-print(f"Original message: {originalmessage}")
-print(f"Encrypted message: {encryptedmessage}")
--- Portfolio/week 6 aditi/week601.py
+++
@@ -1,9 +0,0 @@
-def convertbinary(m):
-    a=[]
-    n=int(m)
-    while n!=0:
-        a.append(n%2)
-        n=n//2
-    a.reverse()
-    return a
-print(*convertbinary(14))
